f1fbce96afe606df7840da2935a7ee5e9f1e0d3e
colin robertson <wobblyyyy@gmail.com>
2021-03-05 19:13:08 -0500

    Update EDT JavaDoc for 1.1.0 release.

--- edt/package-search-index.js
+++ edt/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"me.wobblyyyy.edt"},{"l":"me.wobblyyyy.edt.arrays"},{"l":"me.wobblyyyy.edt.maps"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"me.wobblyyyy.edt"},{"l":"me.wobblyyyy.edt.arrays"},{"l":"me.wobblyyyy.edt.functional"},{"l":"me.wobblyyyy.edt.maps"}]
